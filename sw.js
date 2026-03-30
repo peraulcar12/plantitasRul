7761da255d53affc45e36c9444e1a9a6d561df2b
peraulcar12 <peraulcar12@gmail.com>
2026-03-30 15:15:59 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+﻿const CACHE_NAME = 'jardin-raul-v1';
+const ASSETS = [
+  'index.html',
+  'https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Josefin+Sans:wght@300;400;600;700&display=swap'
+];
+
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
